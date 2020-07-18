d1799747fb01bf48f30ea929f856d599e3c23055
Alex Nagy <therebeloper@gmail.com>
2020-07-18 09:59:40 +0300

    Initial Commit

---
+++ CoreDataFun1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataFun1
+//
+//  Created by Alex Nagy on 18/07/2020.
+//  Copyright © 2020 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
